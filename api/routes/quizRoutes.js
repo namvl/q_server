56e31d694af76261ae76fb4f94139db120a5425f
namvl <namvl@hotmail.com>
2017-10-12 21:55:35 +0900

    Add quizroutes code

---
+++ api/routes/quizRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var quizList = require('../controllers/quizController');
+
+  // todoList Routes
+  app.route('/quizzes')
+    .get(quizList.list_all_quizzes)
+    .post(quizList.create_a_quiz);
+
+
+  app.route('/quizzes/:quizId')
+    .get(quizList.read_a_quiz)
+    .put(quizList.update_a_quiz)
+    .delete(quizList.delete_a_quiz);
+};
