2c1be36ca971715b5ef313959642053723838a13
namvl <namvl@hotmail.com>
2017-10-12 22:00:18 +0900

    Update quiz controller code

--- api/controllers/quizController.js
+++ api/controllers/quizController.js
@@ -10,3 +10,42 @@ exports.list_all_quizzes = function(req, res) {
     res.json(quiz);
   });
 };
+
+
+exports.create_a_quiz = function(req, res) {
+  var new_quiz = new Quiz(req.body);
+  new_quiz.save(function(err, quiz) {
+    if (err)
+      res.send(err);
+    res.json(quiz);
+  });
+};
+
+
+exports.read_a_quiz = function(req, res) {
+  Quiz.findById(req.params.quizId, function(err, quiz) {
+    if (err)
+      res.send(err);
+    res.json(quiz);
+  });
+};
+
+
+exports.update_a_quiz = function(req, res) {
+  Quiz.findOneAndUpdate({_id: req.params.quizId}, req.body, {new: true}, function(err, quiz) {
+    if (err)
+      res.send(err);
+    res.json(quiz);
+  });
+};
+
+
+exports.delete_a_quiz = function(req, res) {
+  Quiz.remove({
+    _id: req.params.quizId
+  }, function(err, quiz) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
