508a9d08799b55f94747b9786a7bb6e53ae062ec
namvl <namvl@hotmail.com>
2017-10-23 20:48:24 +0900

    add config

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 80,
+  port = process.env.PORT || 1234,
   mongoose = require('mongoose'),
   Quiz = require('./api/models/quizModel'),
+  config = require('./config'),
   bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<username>:<password>$@cluster0-shard-00-00-gpfx7.mongodb.net:27017,cluster0-shard-00-01-gpfx7.mongodb.net:27017,cluster0-shard-00-02-gpfx7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+mongoose.connect(config.connectionString);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
